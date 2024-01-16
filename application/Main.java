e05f72e915d625de25014ab09a02b053ff75d93b
Leonardo <leowitcroz@gmail.com>
2024-01-16 20:18:37 -0300

    oddOrEven java

---
+++ application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+
+        if(num % 2 == 0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+};
